2a63514362557087629b73238993672481fd2ad4
Narek Maloyan <narek1110@gmail.com>
2021-08-19 09:46:47 +0300

    effnetb0

--- brain_tumor_classification/model.py
+++ brain_tumor_classification/model.py
@@ -7,12 +7,12 @@ from efficientnet_pytorch_3d import EfficientNet3D
 class BrainTumorClassificationModel(nn.Module):
     def __init__(self, backbone):
         super().__init__()
-        self.net = SEResNet50(in_channels=4, spatial_dims=3,dropout_prob=0.3, num_classes=1)
-        # EfficientNet3D.from_name(
-        #     backbone, override_params={"num_classes": 2}, in_channels=1
-        # )
-        # n_features = self.net._fc.in_features
-        # self.net._fc = nn.Linear(in_features=n_features, out_features=1, bias=True)
+        #self.net = SEResNet50(in_channels=4, spatial_dims=3,dropout_prob=0.3, num_classes=1)
+        self.net = EfficientNet3D.from_name(
+            backbone, override_params={"num_classes": 2}, in_channels=4
+        )
+        n_features = self.net._fc.in_features
+        self.net._fc = nn.Linear(in_features=n_features, out_features=1, bias=True)
 
     def forward(self, x):
         return self.net(x)
